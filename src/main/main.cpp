e8f59ea59eed25e1fc40fb291dd435fc24d3c628
vinicius.cruz <vvinycruz@gmail.com>
2025-09-29 21:40:07 -0300

    added function pulse tasmota

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -2,6 +2,7 @@
 #include "hw/hardware_control.h"
 #include "web/web_server.h"
 #include "main/data_manager.h"
+#include <HTTPClient.h>
 
 void setup() {
   Serial.begin(115200);
@@ -21,6 +22,16 @@ void loop() {
     Serial.println("Cartao lido: " + uid);
     bool authorized = isAuthorized(uid);
     addAccessLog(uid, authorized);
-    // Adicione ação: ex. if (authorized) { digitalWrite(LED_PIN, HIGH); }
+    if (authorized) {
+      HTTPClient http;
+      http.begin("http://192.168.15.75/cm?user=admin&password=hazeng4N&cmnd=Power4%20ON");
+      int code = http.GET();
+      if (code == 200) {
+        Serial.println("Pulso de 500 ms enviado com sucesso via Tasmota.");
+      } else {
+        Serial.println("Erro ao enviar pulso: " + String(code));
+      }
+      http.end();
+    }
   }
 }
